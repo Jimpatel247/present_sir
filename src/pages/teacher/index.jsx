f717943cd5b56ec0fc438d9c16b10954b79a9c4e
07manan <07manan1850@gmail.com>
2023-03-25 14:42:22 +0530

    cahnges

--- src/pages/teacher/index.jsx
+++ src/pages/teacher/index.jsx
@@ -3,7 +3,9 @@ import styles from "../../styles/teacher.module.css";
 
 function index() {
   const [active, setActive] = useState(false);
-  return <div className="page-container"></div>;
+  return (
+    <div className="page-container">{active ? "Active" : "Not Active"}</div>
+  );
 }
 
 export default index;
