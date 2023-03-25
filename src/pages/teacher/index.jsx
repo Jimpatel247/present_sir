3375fa1ad0949215a4c86ee2f863301ea56d10cb
07manan <07manan1850@gmail.com>
2023-03-25 14:45:43 +0530

    acac

--- src/pages/teacher/index.jsx
+++ src/pages/teacher/index.jsx
@@ -4,7 +4,7 @@ import styles from "../../styles/teacher.module.css";
 function index() {
   const [active, setActive] = useState(false);
   return (
-    <div className="page-container">{active ? "Active" : "Not Active"}</div>
+    <div className="page-container">{active ? "Activ" : "Not Active"}</div>
   );
 }
 
