9ffab7877e651cf39ab6ebf7b1cd9d1704311483
07manan <07manan1850@gmail.com>
2023-03-25 12:26:34 +0530

    fix useRouter

--- src/pages/teacher/[classID]/index.jsx
+++ src/pages/teacher/[classID]/index.jsx
@@ -1,9 +1,9 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
 function index() {
-  const router = useRouter();
-  const { classID } = router.query;
-  return <div>{classID}</div>;
+  // const router = useRouter();
+  // const { classID } = router.query;
+  return <div>classID</div>;
 }
 
 export default index;
