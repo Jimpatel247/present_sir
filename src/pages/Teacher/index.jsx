4b473ae6cabe83eb7ad5f8063a58694efe05aaff
07manan <07manan1850@gmail.com>
2023-03-25 14:51:32 +0530

    fix 1

--- src/pages/Teacher/index.jsx
+++ src/pages/Teacher/index.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-function index() {
+function Dash() {
   const [batches, setBatches] = useState([]);
   return <div>Teacher dash</div>;
 }
 
-export default index;
+export default Dash;
