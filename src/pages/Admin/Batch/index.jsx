bb169291aad6f2338566445a1bddd6ed009511a1
07manan <07manan1850@gmail.com>
2023-03-28 14:38:17 +0530

    revert

--- src/pages/Admin/Batch/index.jsx
+++ src/pages/Admin/Batch/index.jsx
@@ -121,7 +121,6 @@ function Batch() {
               <option value="6">6</option>
               <option value="7">7</option>
               <option value="8">8</option>
-              <option value="8">8</option>
             </select>
             <div className={styles.subject}>
               {subjects.map((subject, key) => (
