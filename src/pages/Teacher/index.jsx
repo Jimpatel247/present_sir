ed52633ec1b3c79bc7239f71c7ce5a394fb92964
jimpatel247 <pateljim2002@gmail.com>
2023-04-15 05:16:24 +0530

    id wala ppage

--- src/pages/Teacher/index.jsx
+++ src/pages/Teacher/index.jsx
@@ -2,21 +2,29 @@ import Head from "next/head";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import {auth, db } from "@/firebase/initFirebase";
 import { useAuth } from "context/AuthContext";
-import { useState,useEffect } from "react";
+import { useState,useEffect} from "react";
+import { useRouter } from "next/router";
 
 function TeacherDash() {
-
+  const router = useRouter();
   const {currentUser } = useAuth();
-  const q = query(collection(db, "Teachers"), where("email", "==", currentUser.email));
-  console.log(currentUser);
-  const [classData, setclassData] = useState([]);
-  const getData = async () => {
-    const querySnapshot = await getDocs(q);
-    console.log("isi")
-    querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data().classes);
-});
+  if(!currentUser){
+    alert("Plese Login");
+    router.push("/auth/Login");
+  }
+  
+    const q = query(collection(db, "Teachers"), where("email", "==", currentUser.email));
+    console.log(currentUser);
+    const [classData, setclassData] = useState([]);
+    const getData = async () => {
+      const querySnapshot = await getDocs(q);
+      console.log("isi")
+      querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data().classes);
+  });
+  
+  
   };
 
   useEffect(() => {
