8deea7b2f21d430f636bc2084034030b423db4fe
07manan <07manan1850@gmail.com>
2023-04-16 21:07:11 +0530

    fix

--- src/pages/Student/CheckAttendance.jsx
+++ src/pages/Student/CheckAttendance.jsx
@@ -19,7 +19,7 @@ export default function CheckAttendance() {
       !formData.admissionYr
     ) {
       router.push("/Student");
-    }
+    } // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <div className="page-container">
